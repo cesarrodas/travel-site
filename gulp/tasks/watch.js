9c4c993cb48f3597be9c20a7c3fd628d82d6fef5
Cesar <rodascesar03@yahoo.com>
2016-11-21 21:51:27 -0600

    Completed Gulp build task

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,6 +1,6 @@
-var gulp = require('gulp');
-var watch = require('gulp-watch');
-var browserSync = require('browser-sync').create();
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+browserSync = require('browser-sync').create();
 
 gulp.task('watch', function(){
 
